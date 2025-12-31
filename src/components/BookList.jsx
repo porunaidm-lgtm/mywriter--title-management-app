0a4b0a653103403e9794b4556dbff65882a2708c
porunaidm-lgtm <porunai.dm@gmail.com>
2025-12-31 10:29:00 +0530

    Refactor BookList component for better readability

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { useBooks } from '../context/BooksContext';
 import { useToast } from '../context/ToastContext';
 
@@ -115,3 +114,4 @@ export default function BookList({ books }) {
     </table>
   );
 }
+
